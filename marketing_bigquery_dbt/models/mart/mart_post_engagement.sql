6a31786ffadc370645e3ee0f4677fe4d6f8d5d97
zecakpm <joseformiga@ymail.com>
2025-04-08 14:20:51 +0200

    marts updades

--- marketing_bigquery_dbt/models/mart/mart_post_engagement.sql
+++ marketing_bigquery_dbt/models/mart/mart_post_engagement.sql
@@ -5,7 +5,6 @@ SELECT
     u.full_name,
     u.is_verified,
     p.media_type,
-    p.caption,
     p.likes_count,
     p.comments_count,
     COUNT(c.comment_id) AS total_comments,
@@ -16,5 +15,5 @@ LEFT JOIN {{ ref('dim_users') }} u ON p.user_id = u.user_id
 LEFT JOIN {{ ref('fct_comments') }} c ON p.post_id = c.post_id
 GROUP BY
     p.post_id, p.user_id, u.username, u.full_name, u.is_verified,
-    p.media_type, p.caption, p.likes_count, p.comments_count,
+    p.media_type, p.likes_count, p.comments_count,
     p.post_created_at
